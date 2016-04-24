02a6b8d815a4cebc07345f6307cc58d6adcbd31d
andy <andyrociogtz@gmail.com>
2016-04-23 22:29:09 -0500

    Archivos y Cambios

--- views/normatividad_view.php
+++ views/normatividad_view.php
@@ -10,22 +10,22 @@
       <div class="row text-center slideanim">
         <div class="col-sm-4">
           <div class="thumbnail">
-            <a href="http://www.rppc.guanajuato.gob.mx/files/nrrpp2012.pdf" target="_blank">
+            <a href="../files/nrrpp2012.pdf" target="_blank">
               <img src="../dist/images/reglamento.jpg" alt="CDGTO" width="400" height="300">
-              <p><strong>Reglamento RPP GTO. 2012 Vigente</strong></p>
+              <p><strong>Reglamento RPP GTO. 2012 (Vigente)</strong></p>
             </a>
           </div>
         </div>
         <div class="col-sm-4">
           <div class="thumbnail">
-            <a href="http://www.rppc.guanajuato.gob.mx/files/rrppab.pdf" target="_blank">
+            <a href="../files/rrppab.pdf" target="_blank">
               <img src="../dist/images/reglamentos-696x400.png" alt="Reglamento" width="400" height="300">
-              <p><strong>Reglamento del Registro Público de la Propiedad Abrogado</strong></p>
+              <p><strong>Reglamento del Registro Público de la Propiedad (Abrogado)</strong></p>
             </a>
           </div>
         </div>
         <div class="col-sm-4">
-          <a href="http://www.rppc.guanajuato.gob.mx/files/1.pdf" target="_blank">
+          <a href="../files/1.pdf" target="_blank">
           <div class="thumbnail">
             <img src="../dist/images/infonavit.jpg" alt="Lineamientos" width="200" height="300">
             <p><strong>Lineamientos</strong></p>
@@ -38,7 +38,7 @@
       <div class="row text-center slideanim">
         <div class="col-sm-4">
           <div class="thumbnail">
-            <a href="http://www.congresogto.gob.mx/uploads/ley/pdf/144/Ley_de_Ingresos_para_el_Estado_de_Guanajuato_para_el_Ejercicio_Fiscal__2016.pdf" target="_blank">
+            <a href="../files/Ley_de_Ingresos_para_el_Estado_de_Guanajuato_para_el_Ejercicio_Fiscal__2016.pdf" target="_blank">
               <img src="../dist/images/AAADinero_0.jpg" alt="ingresos" width="280" height="150">
               <p><strong>Ley de Ingresos</strong></p>
               <p>Del Estado de Guanajuato</p>
@@ -48,19 +48,19 @@
 
         <div class="col-sm-4">
           <div class="thumbnail">
-            <a href="  " target="_blank">
+            <a href="../files/criteriosregistrales.pdf" target="_blank">
               <img src="../dist/images/admin_alma.png" alt="ingresos" width="200" height="150">
-              <p><strong>Ley de Ingresos</strong></p>
+              <p><strong>Criterios Registrales</strong></p>
               <p>Del Estado de Guanajuato</p>
             </a>
           </div>
         </div>
 
         <div class="col-sm-4">
-          <a href="http://www.rppc.guanajuato.gob.mx/files/acuerdo-administrativo.pdf" target="_blank">
+          <a href="../files/acuerdo-administrativo.pdf" target="_blank">
           <div class="thumbnail">
             <img src="../dist/images/acuerdo.png" alt="acuerdo" width="150" height="100">
-            <p><strong>Acuerdo Admministrativo cobro de Productos</strong></p>
+            <p><strong>Acuerdo Administrativo cobro de Productos</strong></p>
             <p>Criterios Registrales</p>
           </div>
           </a>
